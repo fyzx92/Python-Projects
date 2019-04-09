7312c173fcc7bde24e5b3443f1cabfb4f47e10ca
fyzx92 <brycebrgss@yahoo.com>
2019-04-09 12:39:04 +0200

    includes probabalistic axioms

--- Projects/Lsystem.py
+++ Projects/Lsystem.py
@@ -1,13 +1,11 @@
-#
 # Authored by Bryce Burgess
 #
 # L system with two characters, a and b
-#
 
 from random import random as rand
 
-def apply_axioms1(string_in, iter = 1):
-    for i in range(iter):
+def apply_axioms1(string_in, itr = 1):
+    for i in range(itr):
         string_out = ""
         for c in string_in:
             if c == "a":
@@ -17,8 +15,8 @@ def apply_axioms1(string_in, iter = 1):
         string_in = string_out
     return string_out
 
-def apply_axioms2(string_in, iter = 1, probA = 0.8, probB = 0.95):
-    for i in range(iter):
+def apply_axioms2(string_in, itr = 1, probA = 0.8, probB = 0.95):
+    for i in range(itr):
         string_out = ""
         for c in string_in:
             if c == "a":
@@ -30,17 +28,22 @@ def apply_axioms2(string_in, iter = 1, probA = 0.8, probB = 0.95):
         string_in = string_out
     return string_out
 
-def gen_start_str (method = "simple", input_str = None, str_length = 3, char_list = ["a","b"]):
+def gen_start_str(method = "simple", input_str = None, str_length = 3, char_list = ["a","b"]):
     char_list = list(set(char_list))
-    valid_input = [False]*len(input_str)
-    for i in input_str:
-        for j in char_list:
-            if i == j:
-                valid_input[i] = True
+    
+    # check input validity
+    if input_str:
+        valid_input = [False]*len(input_str)
+        for i in input_str:
+            for j in char_list:
+                if i == j:
+                    valid_input[i] = True
 
-    if all(valid_input):
-        start_str = input_str
-    else:
+        if all(valid_input):
+            start_str = input_str
+            
+    # Generate start string
+    elif not input_str or not all(valid_input):
         if method == "simple":
             start_str = "aaabbb"
 
@@ -48,8 +51,7 @@ def gen_start_str (method = "simple", input_str = None, str_length = 3, char_lis
             start_str = ""
             for i in range(str_length):
                 next_char = "a"
-                if rand() < 0.5:
-                    next_char = "b"
+                if rand() < 0.5: next_char = "b"
                 start_str += next_char
 
         if method == "manual":
@@ -62,3 +64,4 @@ string = gen_start_str()
 for i in range(20):
     print(string)
     string = apply_axioms1(string)
+    #string = apply_axioms2(string)
